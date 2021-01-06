b9c494b24c7f626cfb7cef4a22ced89398f3ed07
k4l1brx <tr4nquoc@gmail.com>
2021-01-07 04:59:57 +0700

    First

--- config.def.h
+++ config.def.h
@@ -4,20 +4,30 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "SarasaGothic J:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#d5c4a1", "#1d2021" },
+    [SchemeSel] = { "#1d2021", "#83a598" },
+    [SchemeSelHighlight] = { "#ffc978", "#005577" },
+    [SchemeNormHighlight] = { "#ffc978", "#222222" },
+    [SchemeOut] = { "#1d2021", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 5;
+static unsigned int columns    = 5;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 24;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 1;
